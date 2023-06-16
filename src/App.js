48e71dd3eef74e2d972caebfd4bb0a5a365501f8
mizan21 <mizanorrahman398@gmail.com>
2023-06-16 08:15:49 +0600

    [DONE] routes setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { RouterProvider } from "react-router-dom";
+import router from "./Routes/Routes";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
 }
 
 export default App;
--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Navbar from "../Pages/SharedPages/Navbar/Navbar";
+import Footer from "../Pages/SharedPages/Footer/Footer";
+import { Outlet } from "react-router-dom";
 
 const Main = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Main;
