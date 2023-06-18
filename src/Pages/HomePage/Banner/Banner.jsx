59ab49c6f7454cc947e08a63904a55d8295d465e
mizan21 <mizanorrahman398@gmail.com>
2023-06-18 23:06:44 +0600

    banner part edit

--- src/Pages/HomePage/Banner/Banner.jsx
+++ src/Pages/HomePage/Banner/Banner.jsx
@@ -1,91 +1,63 @@
 import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import slider1 from "../../../assets/images/slider1.jpg";
 import slider2 from "../../../assets/images/slider2.jpg";
 import slider3 from "../../../assets/images/slider3.jpg";
 import slider4 from "../../../assets/images/slider4.jpg";
-import slider5 from "../../../assets/images/slider5.jpg";
 
 const Banner = () => {
   return (
-    <div className="carousel w-full h-[250px] md:h-[600px] lg:h-[700px]">
-      <div id="slide1" className="carousel-item relative w-full">
-        <img src={slider1} className="w-full" alt="" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide4" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide2" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
-          <h3 className="text-lg md:text-2xl font-serif">
-            The Best University Of The State
-          </h3>
-          <h1 className="text-3xl md:text-6xl lg:text-8xl font-serif font-bold">
+    <Carousel
+      autoPlay
+      infiniteLoop
+      showArrows={false}
+      showStatus={false}
+      showThumbs={false}
+      interval={3000}
+      transitionTime={500}
+    >
+      <div className="relative w-full h-[250px] md:h-[600px] lg:h-[700px]">
+        <img src={slider1} className="w-full h-full object-cover" alt="" />
+        <div className="absolute inset-0 flex flex-col justify-center items-center text-white">
+          <h1 className="text-5xl md:text-6xl lg:text-8xl font-serif font-bold mb-4">
             Kingstar University
           </h1>
+          <p className="text-xl">The Best University Of The State</p>
         </div>
       </div>
-      <div id="slide2" className="carousel-item relative w-full">
-        <img src={slider2} className="w-full" alt="" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide1" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide3" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-        <div className="absolute top-24 left-[50%] md:top-1/2 md:left-[60%] md:transform -translate-x-1/2 -translate-y-1/2 text-white">
-          <h3 className="text-lg md:text-2xl font-serif mb-3">
-            Be a part of our history
-          </h3>
-          <h1 className="text-2xl md:text-4xl lg:text-6xl font-serif font-bold bg-green-700 px-5 py-3 mb-3">
+      <div className="relative w-full h-[250px] md:h-[600px] lg:h-[700px]">
+        <img src={slider2} className="w-full h-full object-cover" alt="" />
+        <div className="absolute inset-0 flex flex-col justify-center items-center text-white">
+          <h1 className="text-4xl md:text-6xl lg:text-8xl font-serif font-bold mb-4">
             Kingstar University
           </h1>
-          <p className="font-serif pb-5 hidden md:block">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
-            reiciendis nobis corporis repudiandae veniam. Quis, tempore maxime?
-            Cumque aliquid corporis minima illo, sapiente omnis odio distinctio,
-            corrupti laboriosam vel commodi deserunt veritatis pariatur dolorum,
-            impedit eligendi voluptatum numquam recusandae officiis?
+          <p className="text-lg md:text-xl">Be a part of our history</p>
+          <p className="text-base md:text-lg mt-2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae
+            ex elit. Praesent ultrices nisl vitae congue gravida.
           </p>
-          <p className="w-1/3 border-b-8 border-green-700"></p>
-          <span className="border-b-4"></span>
         </div>
       </div>
-      <div id="slide3" className="carousel-item relative w-full">
-        <img src={slider3} className="w-full" alt="" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide2" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide4" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
-          <h1 className="text-9xl font-serif font-bold">Slide 3</h1>
-          <p>Text for slide 3</p>
+      <div className="relative w-full h-[250px] md:h-[600px] lg:h-[700px]">
+        <img src={slider3} className="w-full h-full object-cover" alt="" />
+        <div className="absolute inset-0 flex flex-col justify-center items-center text-white">
+          <h1 className="text-5xl md:text-6xl lg:text-8xl font-serif font-bold mb-4">
+            Slide 3
+          </h1>
+          <p className="text-xl">Text for slide 3</p>
         </div>
       </div>
-      <div id="slide4" className="carousel-item relative w-full">
-        <img src={slider4} className="w-full" alt="" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide3" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide1" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
-          <h1 className="text-9xl font-serif font-bold">Slide 4</h1>
-          <p>Text for slide 4</p>
+      <div className="relative w-full h-[250px] md:h-[600px] lg:h-[700px]">
+        <img src={slider4} className="w-full h-full object-cover" alt="" />
+        <div className="absolute inset-0 flex flex-col justify-center items-center text-white">
+          <h1 className="text-5xl md:text-6xl lg:text-8xl font-serif font-bold mb-4">
+            Slide 4
+          </h1>
+          <p className="text-xl">Text for slide 4</p>
         </div>
       </div>
-    </div>
+    </Carousel>
   );
 };
 
