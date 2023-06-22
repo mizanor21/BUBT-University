d18d0c2257af6c000a169a65a53d9aea83907889
mizan21 <mizanorrahman398@gmail.com>
2023-06-22 18:18:17 +0600

    Countdown some code edit

--- src/Pages/HomePage/Admission/CountDown.jsx
+++ src/Pages/HomePage/Admission/CountDown.jsx
@@ -33,27 +33,27 @@ const Countdown = () => {
   }, []);
 
   return (
-    <div className="grid grid-flow-col gap-5 my-5 text-center auto-cols-max">
+    <div className="grid grid-flow-col gap-5 my-5 font-serif text-center auto-cols-max">
       <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-        <span className="countdown font-mono text-5xl">
+        <span className="countdown font-extrabold text-5xl">
           <span style={{ "--value": countdown.days }}></span>
         </span>
         days
       </div>
       <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-        <span className="countdown font-mono text-5xl">
+        <span className="countdown font-extrabold text-5xl">
           <span style={{ "--value": countdown.hours }}></span>
         </span>
         hours
       </div>
       <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-        <span className="countdown font-mono text-5xl">
+        <span className="countdown font-extrabold text-5xl">
           <span style={{ "--value": countdown.minutes }}></span>
         </span>
         min
       </div>
       <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-        <span className="countdown font-mono text-5xl">
+        <span className="countdown font-extrabold text-5xl">
           <span style={{ "--value": countdown.seconds }}></span>
         </span>
         sec
