8006bb6d79e09a52b544c1ee4bd82fe25849cc1c
mizan21 <mizanorrahman398@gmail.com>
2023-07-02 20:25:19 +0600

    Error page setup

---
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <h1 className="text-5xl text-center">
+        404 <br />- Page Not Found!
+      </h1>
+    </div>
+  );
+};
+
+export default ErrorPage;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -5,6 +5,7 @@ import History from "../Pages/OtherPages/About/History/History";
 import NoticeDetails from "../Pages/HomePage/TabNoticeProgramsEvent/NoticeDetails";
 import EventDetails from "../Pages/HomePage/TabNoticeProgramsEvent/EventDetails";
 import AllStories from "../Pages/HomePage/Alumni/AllStories";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
   {
@@ -37,6 +38,10 @@ const router = createBrowserRouter([
         loader: () => fetch("http://localhost:5000/all-stories"),
       },
     ],
+  },
+  {
+    path: "*",
+    element: <ErrorPage></ErrorPage>,
   },
 ]);
 
