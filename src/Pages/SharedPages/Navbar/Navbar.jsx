0f46cf247ce7da2985f77dc5789c948c3848486e
mizan21 <mizanorrahman398@gmail.com>
2023-07-05 20:54:14 +0600

    navbar redesign

--- src/Pages/SharedPages/Navbar/Navbar.jsx
+++ src/Pages/SharedPages/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
-import logo from "../../../assets/images/logo.png";
+import logo from "../../../assets/images/bubt_logo.png";
 import "./Navbar.css";
 
 const Navbar = () => {
@@ -92,13 +92,13 @@ const Navbar = () => {
         showNavbar ? "show" : "hide"
       }`}
     >
-      <div className="container mx-auto flex items-center justify-between py-4">
+      <div className="container mx-auto flex justify-between py-4">
         <div className="flex items-center">
           <NavLink to={"/"}>
-            <img className="w-52" src={logo} alt="" />
+            <img className="w-48 md:w-64" src={logo} alt="" />
           </NavLink>
         </div>
-        <div className="hidden lg:flex">
+        <div className="hidden lg:flex items-center">
           <ul className="flex space-x-4">
             {navItems.map((item, index) => (
               <li
@@ -133,8 +133,8 @@ const Navbar = () => {
             ))}
           </ul>
         </div>
-        <div className="flex items-center">
-          <button className="btn btn-circle btn-primary mr-4">
+        <div className="flex items-center lg:hidden">
+          {/* <button className="btn btn-circle btn-primary mr-4">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5 text-white"
@@ -149,7 +149,7 @@ const Navbar = () => {
                 d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
               />
             </svg>
-          </button>
+          </button> */}
           <button
             className="btn btn-circle btn-primary lg:hidden"
             onClick={toggleDropdown}
