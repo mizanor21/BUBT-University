d1c9e4ac07243eb9b3da50aea00d4861bdd5ec41
mizan21 <mizanorrahman398@gmail.com>
2023-07-13 20:42:47 +0600

    [DONE] Faculty members part polishing

--- src/Pages/OtherPages/Academic/Faculty/Faculty.jsx
+++ src/Pages/OtherPages/Academic/Faculty/Faculty.jsx
@@ -7,10 +7,10 @@ const Faculty = ({ faculty }) => {
     <div>
       <Link
         to={`/faculty-details/${_id}`}
-        className="card bg-base-100 border-b-8 border-green-600 hover:border-green-700 hover:animate-pulse shadow-xl"
+        className="card bg-base-100 border-b-8 border-green-600 hover:border-green-700 hover:-translate-y-2 hover:duration-500 shadow-xl"
       >
         <figure>
-          <img className="w-full" src={img} alt={name} />
+          <img className="w-full h-[300px]" src={img} alt={name} />
         </figure>
         <div className="card-body p-4 gap-0">
           <h2 className="text-lg font-bold font-serif p-0">{name}</h2>
--- src/Pages/OtherPages/Academic/Faculty/Facultys.jsx
+++ src/Pages/OtherPages/Academic/Faculty/Facultys.jsx
@@ -1,15 +1,33 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
 import Faculty from "./Faculty";
+import image1 from "../../../../assets/images/convocation.jpeg";
 
 const Facultys = () => {
   const facultys = useLoaderData();
   return (
-    <div className="container mx-auto flex justify-center items-center my-10">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
-        {facultys.map((faculty) => (
-          <Faculty key={faculty._id} faculty={faculty}></Faculty>
-        ))}
+    <div>
+      <div className="relative ">
+        <img
+          src={image1}
+          alt="Banner"
+          className="object-cover w-full h-48 md:h-64 "
+        />
+        <div className="absolute inset-0 flex flex-col justify-center items-center bg-opacity-50 bg-gray-900 font-serif">
+          <h1 className="text-white text-2xl md:text-3xl font-bold border-b-4 border-white pb-2">
+            Faculty Members
+          </h1>
+          <h1 className="text-white text-3xl md:text-4xl font-bold">
+            Department of CSE
+          </h1>
+        </div>
+      </div>
+      <div className="container mx-auto flex justify-center items-center my-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
+          {facultys.map((faculty) => (
+            <Faculty key={faculty._id} faculty={faculty}></Faculty>
+          ))}
+        </div>
       </div>
     </div>
   );
